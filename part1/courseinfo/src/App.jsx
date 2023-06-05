343aecf580a6392812753cb36f58691a69beaf08
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-06-05 20:53:36 +0300

    done exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,11 +7,21 @@ const Header = (props) => {
   )
 } 
 
-const Content = (props) => {
+const Part = (props) => {
   return(
     <p>
-      {props.part} {props.exercises}
+    {props.parts[props.index]} {props.exercises[props.index]}
     </p>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <Part parts={props.parts} exercises={props.exercises} index={0}/>
+      <Part parts={props.parts} exercises={props.exercises} index={1}/>
+      <Part parts={props.parts} exercises={props.exercises} index={2}/>
+    </div>
   )
 }
 
@@ -36,9 +46,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]} />
       <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
