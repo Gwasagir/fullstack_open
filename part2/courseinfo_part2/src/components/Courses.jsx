2c1ceaa28f4cd21dda4fc50792e48eae153449f9
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-06-07 14:46:18 +0300

    Exc 2.5 stuck at unique 'key'

---
+++ part2/courseinfo_part2/src/components/Courses.jsx
@@ -0,0 +1,34 @@
+const Part = (props) => {
+    return(
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+    )}
+  
+const Content = (props) => {
+    return(props.parts.map(part => <Part key={part.id} part={part}/>)
+    )}
+  
+const Total = (props) => {
+    let calcTotal = props.parts.reduce((sum, order) => {
+      return sum + order.exercises
+    }, 0)
+    return(
+      <b>
+        total of {calcTotal} exercises
+      </b>
+      )}
+
+const Header = ({name}) => <h2>{name}</h2>
+
+const Courses = (props) => {
+    return(props.courses.map(courses =>
+      <>
+        <div> <Header key={courses.name} name={courses.name} /> </div>
+        <div> <Content key={"course"+courses.id} parts={courses.parts} /> </div>
+        <div> <Total key={"totalkey"+courses.id} parts={courses.parts} /> </div>
+      </> 
+      ))
+  }
+
+export default Courses
